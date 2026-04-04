55c852f71cf90f3886eae8ebe4831f98f8aeaf68
Bharadwaj Talabattula <bharawajtalabattula@gmail.com>
2026-04-04 22:56:26 +0530

    fix: server URL

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL,
+    baseURL: 'https://chatflow-7can.onrender.com/api'
 })
 
 apiClient.interceptors.request.use((config) => {
