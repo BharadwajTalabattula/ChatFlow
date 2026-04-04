9bc3675a9975154abbb44b4ac5583b4fefeab198
Bharadwaj Talabattula <bharawajtalabattula@gmail.com>
2026-04-04 22:35:34 +0530

    temp rename

--- client/src/features/chats/pages/chatPage.jsx
+++ client/src/features/chats/pages/chatPage.jsx
@@ -1,7 +1,8 @@
 
 import ChatWindow from "../components/chatWindow";
 import MessageInput from "../components/messageInput";
-import Layout from "../../../shared/components/Layout";
+import Layout from "../../../shared/components/LayoutTemp";
+
 
 export default function ChatPage() {
   return (
