f7bf3233bd5da12e25a92c7a40829bc90796fb3a
Bharadwaj Talabattula <bharawajtalabattula@gmail.com>
2026-04-04 19:30:28 +0530

    fix: import case sensitivity

--- client/src/features/chats/pages/chatPage.jsx
+++ client/src/features/chats/pages/chatPage.jsx
@@ -1,12 +1,18 @@
-import Layout from "../../../shared/components/layout"; 
+
 import ChatWindow from "../components/ChatWindow";
 import MessageInput from "../components/MessageInput";
+import Layout from "../../../shared/components/Layout";
 
 export default function ChatPage() {
   return (
-    <Layout >
-      <ChatWindow />
-      <MessageInput />
-    </Layout>
-  );
+    <>
+ <Layout>
+    <ChatWindow />
+    <MessageInput />
+  </Layout> 
+    </>
+  
+    
+   
+  )
 }
